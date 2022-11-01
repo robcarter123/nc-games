7d3d1809043ea439a24e6a0690fc9364937768dc
robcarter123 <robcarter@macbook-pro.home>
2022-11-01 15:22:18 +0000

    refactored per advisement

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import Header from "./Components/Header";
 import './App.css'
 import ReviewCollection from "./Components/ReviewCollection"
-import CardExample from "./Components/ReviewCollection"
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <CardExample />
+      <ReviewCollection />
     </div>
   );
 }
